0ff8b86edf3fb991330f01bc93860366de524a5e
Carlos <carlosfdezvivas98@gmail.com>
2025-01-15 17:09:44 +0100

    carpeta de juegos

--- caldero de agua/core/src/main/java/io/github/some_example_name/Main.java
+++ caldero de agua/core/src/main/java/io/github/some_example_name/Main.java
@@ -3,14 +3,19 @@ package io.github.some_example_name;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import java.util.Vector;
 
 /**
  * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
@@ -25,6 +30,8 @@ public class Main extends ApplicationAdapter implements ApplicationListener {
     Music music;
     SpriteBatch spriteBatch;
     FitViewport viewport;
+    Sprite bucketSprite;
+    Vector2 touchPos;
 
     @Override
     public void create() {
@@ -35,6 +42,9 @@ public class Main extends ApplicationAdapter implements ApplicationListener {
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
         spriteBatch = new SpriteBatch();
         viewport = new FitViewport(8, 5);
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(1, 1);
+        touchPos = new Vector2();
     }
 
     @Override
@@ -51,6 +61,21 @@ public class Main extends ApplicationAdapter implements ApplicationListener {
     }
 
     private void input() {
+        float speed = 2f;
+        float delta = Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucketSprite.translateX(speed * delta);
+        } else {
+            if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+                bucketSprite.translateX(-speed * delta);
+            }
+        }
+        if (Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(),Gdx.input.getY());
+            viewport.unproject(touchPos);
+            bucketSprite.setCenterX(touchPos.x);
+
+        }
 
     }
 
@@ -63,6 +88,16 @@ public class Main extends ApplicationAdapter implements ApplicationListener {
         viewport.apply();
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
         spriteBatch.begin();
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+//
+//        spriteBatch.draw(bucketTexture,0,0,1,1);
+
+        bucketSprite.draw(spriteBatch);
 
         spriteBatch.end();
     }
