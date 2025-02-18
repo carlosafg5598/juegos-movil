0c7e49403cca43a0a04af9d894b35e5028a42c34
Carlos <carlosfdezvivas98@gmail.com>
2025-02-18 20:59:22 +0100

    movimiento esquiador

--- SkiSurvival/core/src/main/java/Sprites/Esquiador.java
+++ SkiSurvival/core/src/main/java/Sprites/Esquiador.java
@@ -1,14 +1,10 @@
 package Sprites;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Esquiador extends Sprite {
@@ -22,62 +18,17 @@ public class Esquiador extends Sprite {
 
     public void defineEsquiador() {
         BodyDef bdef = new BodyDef();
-        bdef.position.set(32, 100);
+        bdef.position.set(150, 1500);  // Empieza en una posición en la que puedas ver al esquiador
         bdef.type = BodyDef.BodyType.DynamicBody;
         body = world.createBody(bdef);
         FixtureDef fdef = new FixtureDef();
         CircleShape shape = new CircleShape();
-        shape.setRadius(5);
+        shape.setRadius(5); // Tamaño del esquiador
         fdef.shape = shape;
         body.createFixture(fdef);
     }
 
-
+    public void move(float x, float y) {
+        body.setLinearVelocity(x, y); // Esto mueve al esquiador
+    }
 }
-//    private ShapeRenderer shapeRenderer;
-//    private Texture texture;
-//    private Sprite sprite;
-//
-//    public Esquiador(World world, float x, float y){
-//        this.world=world;
-//        shapeRenderer = new ShapeRenderer();
-//        createBody(x,y);
-//
-//    }
-//    private void createBody(float x, float y){
-//        BodyDef bdef= new BodyDef();
-//        bdef.type= BodyDef.BodyType.DynamicBody;
-//        bdef.position.set(x,y);
-//
-//        b2dy=world.createBody(bdef);
-//        PolygonShape shape= new PolygonShape();
-//        shape.setAsBox(20,20);
-//        FixtureDef fixtureDef= new FixtureDef();
-//        fixtureDef.shape = shape;
-//        fixtureDef.density = 1f;
-//        fixtureDef.friction = 0.5f;
-//        b2dy.createFixture(fixtureDef);
-//        shape.dispose();
-//
-//    }
-//
-//    public void draw() {
-//        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-//        shapeRenderer.setColor(0, 1, 0, 1); // Color verde
-//        shapeRenderer.rect(b2dy.getPosition().x - 20, b2dy.getPosition().y - 20, 40, 40);
-//        shapeRenderer.end();
-//    }
-//
-//    public void move(float x, float y) {
-//        b2dy.setLinearVelocity(new Vector2(x, y));
-//    }
-//
-//    public float getY() {
-//        return b2dy.getPosition().y;
-//    }
-//
-//    public void dispose() {
-//        shapeRenderer.dispose();
-//    }
-
-
--- SkiSurvival/core/src/main/java/io/github/some_example_name/GameScreen.java
+++ SkiSurvival/core/src/main/java/io/github/some_example_name/GameScreen.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.MapObject;
@@ -16,17 +15,20 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactListener;
 
+import Sprites.Esquiador;
 public class GameScreen extends InputAdapter implements Screen {
 
     private final Main game;
@@ -36,15 +38,16 @@ public class GameScreen extends InputAdapter implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
-
     private BitmapFont font;
     private final float WORLD_WIDTH = 350;
     private final float WORLD_HEIGHT = 400;
 
-    //Box2D Variables
+    // Box2D Variables
     private World world;
     private Box2DDebugRenderer b2dr;
 
+    // Esquiador y su body
+    private Esquiador esquiador;
 
     GameScreen(Main game) {
         this.game = game;
@@ -59,8 +62,10 @@ public class GameScreen extends InputAdapter implements Screen {
         renderer = new OrthogonalTiledMapRenderer(map);
 
         camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
-        world = new World(new Vector2(0, 0), true);
+        world = new World(new Vector2(0, -9.8f), true); // Gravedad hacia abajo
         b2dr = new Box2DDebugRenderer();
+
+        // Crear obstáculos del mapa (árboles, etc)
         BodyDef bodyDef = new BodyDef();
         PolygonShape shape = new PolygonShape();
         FixtureDef fixtureDef = new FixtureDef();
@@ -75,34 +80,74 @@ public class GameScreen extends InputAdapter implements Screen {
             body.createFixture(fixtureDef);
         }
 
+        // Crear el esquiador
+        esquiador = new Esquiador(world);
+
+        // Configurar el listener de contacto para detectar colisiones
+        world.setContactListener(new ContactListener() {
+            @Override
+            public void beginContact(Contact contact) {
+                System.out.println("¡Colisión detectada!");
+            }
+
+            @Override
+            public void endContact(Contact contact) {
+                // Aquí se puede manejar el final de la colisión si es necesario
+            }
+
+            @Override
+            public void preSolve(Contact contact, Manifold oldManifold) {
+                // Lógica previa a la colisión
+            }
+
+            @Override
+            public void postSolve(Contact contact, ContactImpulse impulse) {
+                // Lógica posterior a la colisión
+            }
+        });
     }
 
+
+
     public void handleInput(float dt) {
+        float screenWidth = Gdx.graphics.getWidth(); // Ancho de la pantalla
+        float screenHeight = Gdx.graphics.getHeight(); // Altura de la pantalla
         if (Gdx.input.isTouched()) {
+
             camera.position.y -= 100 * dt;
+            int screenX= Gdx.input.getX();
+            int screenY=Gdx.input.getY();
+
+            if (screenX > screenWidth / 2) {
+                // Movimiento a la derecha (agregar velocidad positiva en el eje X)
+                esquiador.move(20, esquiador.body.getLinearVelocity().y);  // Movimiento constante hacia la derecha
+            } else {
+                // Movimiento a la izquierda (agregar velocidad negativa en el eje X)
+                esquiador.move(-20, esquiador.body.getLinearVelocity().y);  // Movimiento constante hacia la izquierda
+            }
         }
     }
 
     public void update(float dt) {
         handleInput(dt);
-        world.step(1/60f,6,2);
+        world.step(1 / 60f, 6, 2);
+
+        System.out.println("Esquiador X: " + esquiador.body.getPosition().x + ", Esquiador Y: " + esquiador.body.getPosition().y);
+        // Actualizar la posición de la cámara para que siga al esquiador
+        camera.position.set(esquiador.body.getPosition().x, esquiador.body.getPosition().y, 0);
         camera.update();
+
         renderer.setView((OrthographicCamera) camera);
     }
 
-
     @Override
     public void render(float delta) {
-
         update(delta);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         renderer.render();
         b2dr.render(world, camera.combined);
-
-
     }
-
 
     @Override
     public void resize(int width, int height) {
@@ -111,29 +156,20 @@ public class GameScreen extends InputAdapter implements Screen {
     }
 
     @Override
-    public void pause() {
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
         game.batch.dispose();
-        //background.dispose();
         font.dispose();
     }
 
     @Override
-    public void show() {
-
-    }
+    public void show() {}
 }
